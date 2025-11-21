4aca8a4fc836d0b50b091dd29373df457d413127
Juan Reina <juanreins48@gmail.com>
2025-11-21 01:21:13 -0500

    Update API_URL to Render backend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     setLoading(true);
 
     try {//cambiar el api
-      const res = await fetch("http://localhost:4000/api/auth/login", {
+      const res = await fetch("http://uconecta.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
